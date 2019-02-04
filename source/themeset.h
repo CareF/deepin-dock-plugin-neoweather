ff7e6dbb1617a1d72db34a18cb3e25c76ee78b98
CareF <CareF.Lm@gmail.com>
2019-02-04 11:38:24 -0500

    set new theme

--- source/themeset.h
+++ source/themeset.h
@@ -2,6 +2,13 @@
 #define THEMESET_H
 #include <QStringList>
 
-const QStringList themeSet({"hty", "gray"});
+/** \brief themeSet is a list describing the possible choices of icon themes.
+ * The first String is the default choice.
+ * The theme names should be consistent with the corresponding definition of
+ * the prefix in `res.qrc` file.
+ * In the `res.qrc` file it's expected to set alias of the icon codes:
+ * `01d`, `01n`, ..., `04n`, `09d`, ..., `13d`, `13n`, `50d`, `50n`, `na`
+ */
+const QStringList themeSet({"neoGray", "neoColor", "hty"});
 
 #endif // THEMESET_H
