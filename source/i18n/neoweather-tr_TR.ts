d11a1e8af2800513e7143a43a1956865da72591c
Caglar Turali <caglarturali@gmail.com>
2019-03-18 03:00:48 +0300

    Add Turkish translation

---
+++ source/i18n/neoweather-tr_TR.ts
@@ -0,0 +1,198 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="tr_TR" sourcelanguage="en">
+<context>
+    <name>ForecastApplet</name>
+    <message>
+        <location filename="../forecastapplet.cpp" line="99"/>
+        <location filename="../forecastapplet.cpp" line="101"/>
+        <location filename="../forecastapplet.cpp" line="109"/>
+        <location filename="../forecastapplet.cpp" line="111"/>
+        <source>N/A</source>
+        <translation>N/A</translation>
+    </message>
+    <message>
+        <location filename="../forecastapplet.cpp" line="107"/>
+        <source>Error: %1</source>
+        <translation>Hata: %1</translation>
+    </message>
+</context>
+<context>
+    <name>WeatherClient</name>
+    <message>
+        <location filename="../weatherclient.cpp" line="73"/>
+        <source>NA</source>
+        <translation>NA</translation>
+    </message>
+    <message>
+        <location filename="../weatherclient.cpp" line="74"/>
+        <source>Clear</source>
+        <translation>Açık</translation>
+    </message>
+    <message>
+        <location filename="../weatherclient.cpp" line="75"/>
+        <source>Rain</source>
+        <translation>Yağmurlu</translation>
+    </message>
+    <message>
+        <location filename="../weatherclient.cpp" line="76"/>
+        <source>Mist</source>
+        <translation>Sisli</translation>
+    </message>
+    <message>
+        <location filename="../weatherclient.h" line="153"/>
+        <source>Humidity: %1%
+</source>
+        <translation>Nem: %1%
+</translation>
+    </message>
+    <message>
+        <location filename="../weatherclient.h" line="154"/>
+        <source>Wind: %1%2, %3°
+</source>
+        <translation>Rüzgâr: %1%2, %3°
+</translation>
+    </message>
+    <message>
+        <location filename="../weatherclient.h" line="156"/>
+        <source>Last Check: %3</source>
+        <translation>Son Denetleme: %3</translation>
+    </message>
+</context>
+<context>
+    <name>WeatherPlugin</name>
+    <message>
+        <location filename="../weatherplugin.cpp" line="42"/>
+        <source>Checking... </source>
+        <translation>Denetleniyor... </translation>
+    </message>
+    <message>
+        <location filename="../weatherplugin.cpp" line="146"/>
+        <source>Settings</source>
+        <translation>Ayarlar</translation>
+    </message>
+    <message>
+        <location filename="../weatherplugin.cpp" line="152"/>
+        <source>Refresh</source>
+        <translation>Yenile</translation>
+    </message>
+    <message>
+        <location filename="../weatherplugin.cpp" line="159"/>
+        <source>Show Log</source>
+        <translation>Günlüğü Göster</translation>
+    </message>
+    <message>
+        <location filename="../weatherplugin.cpp" line="166"/>
+        <source>About</source>
+        <translation>Hakkında</translation>
+    </message>
+    <message>
+        <location filename="../weatherplugin.cpp" line="206"/>
+        <source>About NeoWeather</source>
+        <translation>NeoWeather Hakkında</translation>
+    </message>
+    <message>
+        <location filename="../weatherplugin.cpp" line="207"/>
+        <source>Deepin Dock Plugin: NeoWeather</source>
+        <translation>Deepin Uygulama Eki: NeoWeather</translation>
+    </message>
+    <message>
+        <location filename="../weatherplugin.cpp" line="210"/>
+        <source>Author: CareF &lt;me@mail.caref.xyz&gt;</source>
+        <translation>Geliştirici: CareF &lt;me@mail.caref.xyz&gt;</translation>
+    </message>
+    <message>
+        <location filename="../weatherplugin.cpp" line="212"/>
+        <source>Source Code (GitHub)</source>
+        <translation>Kaynak Kodu (GitHub)</translation>
+    </message>
+    <message>
+        <location filename="../weatherplugin.cpp" line="215"/>
+        <source>License: GPLv3.0</source>
+        <translation>Lisans: GPLv3.0</translation>
+    </message>
+    <message>
+        <location filename="../weatherplugin.h" line="57"/>
+        <source>NeoWeather</source>
+        <translation>NeoWeather</translation>
+    </message>
+</context>
+<context>
+    <name>WeatherSettingDialog</name>
+    <message>
+        <location filename="../weathersettingdialog.cpp" line="88"/>
+        <source>City Information</source>
+        <translation>Şehir Bilgisi</translation>
+    </message>
+    <message>
+        <location filename="../weathersettingdialog.cpp" line="90"/>
+        <source>City</source>
+        <translation>Şehir</translation>
+    </message>
+    <message>
+        <location filename="../weathersettingdialog.cpp" line="95"/>
+        <source>Country</source>
+        <translation>Ülke</translation>
+    </message>
+    <message>
+        <location filename="../weathersettingdialog.cpp" line="107"/>
+        <source>You can also input cityid from &lt;a href=&quot;https://openweathermap.org/city&quot; style=&quot;color: yellow&quot;&gt;openweathermap.org&lt;/a&gt; (the number in the URL of your city page) to avoid unambiguous result. </source>
+        <translation>Kesin olmayan sonuçlardan kaçınmak için &lt;a href=&quot;https://openweathermap.org/city&quot; style=&quot;color: yellow&quot;&gt;openweathermap.org&lt;/a&gt; adresinden Şehir Id&apos;si (şehrinizin sayfasının adres çubuğundaki numara) girebilirsiniz. </translation>
+    </message>
+    <message>
+        <location filename="../weathersettingdialog.cpp" line="122"/>
+        <source>Options</source>
+        <translation>Seçenekler</translation>
+    </message>
+    <message>
+        <location filename="../weathersettingdialog.cpp" line="124"/>
+        <source>Intervel</source>
+        <translation>Güncelleme Aralığı</translation>
+    </message>
+    <message>
+        <location filename="../weathersettingdialog.cpp" line="129"/>
+        <source>999 mi\n</source>
+        <translation>999 mi\n</translation>
+    </message>
+    <message>
+        <location filename="../weathersettingdialog.cpp" line="132"/>
+        <source>Due to the limitations of openweathermap.org for free account, you have to use your own appid to set shorter time interval.</source>
+        <translation>Openweathermap.org ücretsiz hesabı kısıtlamaları yüzünden, daha kısa aralıklarla güncelleme almak için kendi uygulama id&apos;nizi (appid) almalısınız.</translation>
+    </message>
+    <message>
+        <location filename="../weathersettingdialog.cpp" line="136"/>
+        <source>Units</source>
+        <translation>Birimler</translation>
+    </message>
+    <message>
+        <location filename="../weathersettingdialog.cpp" line="138"/>
+        <source>metric</source>
+        <translation>metrik</translation>
+    </message>
+    <message>
+        <location filename="../weathersettingdialog.cpp" line="139"/>
+        <source>imperial</source>
+        <translation>emperyal</translation>
+    </message>
+    <message>
+        <location filename="../weathersettingdialog.cpp" line="150"/>
+        <source>Theme</source>
+        <translation>Tema</translation>
+    </message>
+    <message>
+        <location filename="../weathersettingdialog.cpp" line="157"/>
+        <source>appid from openweathermap.org</source>
+        <translation>openweathermap.org uygulama id&apos;si (appid)</translation>
+    </message>
+    <message>
+        <location filename="../weathersettingdialog.cpp" line="163"/>
+        <source>AppID</source>
+        <translation>AppID</translation>
+    </message>
+    <message>
+        <location filename="../weathersettingdialog.cpp" line="170"/>
+        <source>The plugin default appid is proviede by the author. Click to learn how to get your own appid.</source>
+        <translation>Eklenti için varsayılan appid&apos;si geliştirici tarafından sağlanmıştır. Kendi appid&apos;nizi nasıl alacağınızı öğrenmek için tıklayın.</translation>
+    </message>
+</context>
+</TS>
